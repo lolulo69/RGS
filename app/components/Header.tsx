8268c2d5421ae96895e18b5363512ba687b9b923
lolulo69 <lefortlucas1@gmail.com>
2024-03-21 02:16:03 +0100

    Added shadcn components V0 of drawer on mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import logo from "../images/logo.png";
+import { NavDrawer } from "./NavDrawer";
 
 export const Header = () => {
   return (
@@ -36,22 +37,7 @@ export const Header = () => {
         </a>
       </nav>
       <div className="flex w-[40px] justify-end">
-        <button className="text-gray-200 md:hidden">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16m-7 6h7"
-            />
-          </svg>
-        </button>
+        <NavDrawer />
       </div>
     </header>
   );
---
+++ app/components/NavDrawer.tsx
@@ -0,0 +1,57 @@
+import {
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerPortal,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+export const NavDrawer = () => {
+  return (
+    <Drawer direction="right">
+      <DrawerTrigger className="text-gray-100">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          stroke-width="2"
+        >
+          <path d="M4 6h16M4 12h16m-16 6h16" />
+        </svg>
+      </DrawerTrigger>
+      <DrawerOverlay className="fixed inset-0 bg-sky-950/40" />
+      <DrawerPortal>
+        <DrawerContent className="fixed bottom-0 right-0 mt-24 flex h-full w-[400px] flex-col rounded-t-[10px] bg-sky-950">
+          <div className="flex flex-col gap-4 p-4">
+            <a
+              href="/"
+              className="text-lg font-medium text-gray-100 hover:text-white"
+            >
+              {`Accueil`}
+            </a>
+            <a
+              href="#"
+              className="text-lg font-medium text-gray-100 hover:text-white"
+            >
+              {`Services`}
+            </a>
+            <a
+              href="#projets"
+              className="text-lg font-medium text-gray-100 hover:text-white"
+            >
+              {`Projets`}
+            </a>
+            <a
+              href="#"
+              className="text-lg font-medium text-gray-100 hover:text-white"
+            >
+              {`Contact`}
+            </a>
+          </div>
+        </DrawerContent>
+      </DrawerPortal>
+    </Drawer>
+  );
+};
