2cf954f18d703fef45757122c8b0682ce809f6d7
lolulo69 <lefortlucas1@gmail.com>
2024-04-12 09:16:50 +0200

    Made header design dynamic

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,12 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import logo from "../images/logo.png";
 import { NavDrawer } from "./NavDrawer";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
+  const pathname = usePathname();
   return (
     <header className="mx-auto mb-8 flex flex-row items-center justify-between bg-sky-950 p-3 px-6 md:my-8 md:max-w-[90%] md:animate-in xl:max-w-7xl">
       <div className="flex flex-row items-center gap-2">
@@ -13,25 +16,25 @@ export const Header = () => {
       <nav className="hidden flex-row gap-0 md:flex ">
         <a
           href="/"
-          className="rounded-sm p-3 px-5 font-medium text-gray-200 transition-colors ease-in hover:bg-sky-900"
+          className={`rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900 ${pathname === "/" ? "font-medium" : ""}`}
         >
           Accueil
         </a>
         <a
           href="/actualites"
-          className="rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900"
+          className={`rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900 ${pathname === "/actualites" ? "font-medium" : ""}`}
         >
           Actualités
         </a>
         <a
           href="/projets"
-          className="rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900"
+          className={`rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900 ${pathname === "/projets" ? "font-medium" : ""}`}
         >
           Projets
         </a>
         <a
           href="/contact"
-          className="rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900"
+          className={`rounded-sm p-3 px-5 text-gray-200 transition-colors ease-in hover:bg-sky-900 ${pathname === "/contact" ? "font-medium" : ""}`}
         >
           Contact
         </a>
