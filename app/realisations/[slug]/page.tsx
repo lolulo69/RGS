096554598f85e1a916bfc9e776c74826f4bf66ab
lolulo69 <lefortlucas1@gmail.com>
2025-01-16 19:25:26 +0100

    Back to server component for the realisation page

--- app/realisations/[slug]/page.tsx
+++ app/realisations/[slug]/page.tsx
@@ -1,14 +1,7 @@
-"use client";
+import { detectImageGroups } from "@/app/components/GroupImagesAndDisplayCarousel";
 import { fetchOnePost } from "@/utils/fetchPosts";
 import Image from "next/image";
 import React from "react";
-import parse, { Element } from "html-react-parser";
-import Slider from "react-slick";
-
-type ImageData = {
-  src: string;
-  alt: string;
-};
 
 type Post = {
   title: string;
@@ -20,70 +13,6 @@ type Params = {
   params: {
     slug: string;
   };
-};
-
-const detectImageGroups = (htmlContent: string): React.ReactNode => {
-  const parsedContent = parse(htmlContent, {
-    replace: (domNode) => {
-      if (
-        domNode instanceof Element &&
-        domNode.tagName === "figure" &&
-        domNode.attribs.class?.includes("wp-block-gallery")
-      ) {
-        const images = Array.from(domNode.children)
-          .filter(
-            (child) => child instanceof Element && child.tagName === "figure",
-          )
-          .map((figure) => {
-            const img = (figure as Element).children.find(
-              (child) => child instanceof Element && child.tagName === "img",
-            ) as Element | undefined;
-            return img
-              ? {
-                  src: img.attribs.src,
-                  alt: img.attribs.alt || "",
-                }
-              : null;
-          })
-          .filter((image): image is ImageData => image !== null);
-
-        return <Carousel images={images} />;
-      }
-      return domNode;
-    },
-  });
-
-  return parsedContent;
-};
-
-const Carousel: React.FC<{ images: ImageData[] }> = ({ images }) => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-  };
-
-  return (
-    <Slider {...settings} className="mx-auto my-2 flex w-full max-w-3xl">
-      {images.map((image, index) => (
-        <div
-          key={index}
-          className="flex h-96 items-center justify-center" // Ensures vertical centering
-        >
-          <Image
-            src={image.src}
-            alt={image.alt}
-            width={800}
-            height={400}
-            className="max-h-full w-full object-contain"
-          />
-        </div>
-      ))}
-    </Slider>
-  );
 };
 
 const page: React.FC<Params> = async ({ params }) => {
