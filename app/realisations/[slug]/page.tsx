81ef7d3ec5d2ac5a2eb193c43468ba535809bbd1
lolulo69 <lefortlucas1@gmail.com>
2025-01-16 17:51:17 +0100

    Added carousel in realisations slug page

--- app/realisations/[slug]/page.tsx
+++ app/realisations/[slug]/page.tsx
@@ -1,9 +1,94 @@
+"use client";
 import { fetchOnePost } from "@/utils/fetchPosts";
 import Image from "next/image";
 import React from "react";
+import parse, { Element } from "html-react-parser";
+import Slider from "react-slick";
 
-const page = async ({ params }: { params: { slug: string } }) => {
-  const post = await fetchOnePost(params.slug);
+type ImageData = {
+  src: string;
+  alt: string;
+};
+
+type Post = {
+  title: string;
+  content: string;
+  featured_image: string;
+};
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+const detectImageGroups = (htmlContent: string): React.ReactNode => {
+  const parsedContent = parse(htmlContent, {
+    replace: (domNode) => {
+      if (
+        domNode instanceof Element &&
+        domNode.tagName === "figure" &&
+        domNode.attribs.class?.includes("wp-block-gallery")
+      ) {
+        const images = Array.from(domNode.children)
+          .filter(
+            (child) => child instanceof Element && child.tagName === "figure",
+          )
+          .map((figure) => {
+            const img = (figure as Element).children.find(
+              (child) => child instanceof Element && child.tagName === "img",
+            ) as Element | undefined;
+            return img
+              ? {
+                  src: img.attribs.src,
+                  alt: img.attribs.alt || "",
+                }
+              : null;
+          })
+          .filter((image): image is ImageData => image !== null);
+
+        return <Carousel images={images} />;
+      }
+      return domNode;
+    },
+  });
+
+  return parsedContent;
+};
+
+const Carousel: React.FC<{ images: ImageData[] }> = ({ images }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+  };
+
+  return (
+    <Slider {...settings} className="mx-auto my-2 flex w-full max-w-3xl">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className="flex h-96 items-center justify-center" // Ensures vertical centering
+        >
+          <Image
+            src={image.src}
+            alt={image.alt}
+            width={800}
+            height={400}
+            className="max-h-full w-full object-contain"
+          />
+        </div>
+      ))}
+    </Slider>
+  );
+};
+
+const page: React.FC<Params> = async ({ params }) => {
+  const post: Post = await fetchOnePost(params.slug);
+
   return (
     <main className="mx-auto mb-32 flex max-w-[90%] flex-col xl:max-w-7xl">
       <h1
@@ -16,10 +101,9 @@ const page = async ({ params }: { params: { slug: string } }) => {
             className="hidden text-left font-title text-4xl leading-tight text-gray-800 lg:inline-block lg:text-6xl"
             dangerouslySetInnerHTML={{ __html: post.title }}
           ></h1>
-          <div
-            className="text-sm text-gray-600 lg:text-base"
-            dangerouslySetInnerHTML={{ __html: post.content }}
-          ></div>
+          <div className="text-sm text-gray-600 lg:text-base">
+            {detectImageGroups(post.content)}
+          </div>
         </div>
 
         <Image
